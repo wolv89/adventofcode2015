6d6f2f417d53789d87fa9c306109ff0194de6d07
Peter <peter.delafosse@gmail.com>
2025-01-23 19:57:46 +1100

    Day 2 - Puzzle 2

--- day02/day02.go
+++ day02/day02.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -22,6 +23,16 @@ func (g Gift) MeasureWrappingPaper() int {
 	s1, s2, s3 := g.l*g.w, g.l*g.h, g.w*g.h
 
 	return 2*s1 + 2*s2 + 2*s3 + min(s1, s2, s3)
+
+}
+
+func (g Gift) MeasureRibbon() int {
+
+	sides := []int{g.l, g.w, g.h}
+
+	slices.Sort(sides)
+
+	return 2*sides[0] + 2*sides[1] + g.l*g.w*g.h
 
 }
 
@@ -71,5 +82,46 @@ func (d AocDay2) Puzzle1(useSample int) {
 }
 
 func (d AocDay2) Puzzle2(useSample int) {
+
+	datafile := DIR + "data.txt"
+	if useSample == 1 {
+		datafile = DIR + "sample.txt"
+	}
+
+	f, err := os.Open(datafile)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+
+	var (
+		gift      Gift
+		line      string
+		rb, total int
+	)
+
+	for scanner.Scan() {
+
+		line = scanner.Text()
+
+		dimensions := strings.Split(line, "x")
+
+		gift = Gift{}
+		gift.l, _ = strconv.Atoi(dimensions[0])
+		gift.w, _ = strconv.Atoi(dimensions[1])
+		gift.h, _ = strconv.Atoi(dimensions[2])
+
+		rb = gift.MeasureRibbon()
+		total += rb
+
+		fmt.Println("[", gift.l, "x", gift.w, "x", gift.h, "] | ", rb, " | ", total)
+
+	}
+
+	fmt.Println("")
+	fmt.Println("Total: ", total)
 
 }
